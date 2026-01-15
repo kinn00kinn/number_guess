9eda0c8f01024b69c4bfaf9eeb1814818b403e6c
kinn00kinn <haruki1009kk@gmail.com>
2026-01-15 13:25:59 +0900

    favicon.icoを追加

--- my-algo-web/app/layout.tsx
+++ my-algo-web/app/layout.tsx
@@ -44,6 +44,7 @@ export const metadata: Metadata = {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.ico", sizes: "64x64", type: "image/svg" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
